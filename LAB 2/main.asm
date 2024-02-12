961fe32483a497e602c62cec8191d79e2a3de5d6
PhantomAEF <157931433+PhantomAEF@users.noreply.github.com>
2024-02-11 22:30:56 -0600

    POST LAB v1
    Congelacion en el conteo del timer luego de usar cualquier boton para el conteo del 7 segmentos. Sin embargo sigue contando el 7 seg por lo que no es un bloqueo en el programa completo solo en el timer

--- LAB 2/main.asm
+++ LAB 2/main.asm
@@ -41,7 +41,7 @@ Setup:
 	OUT DDRB, R17
 	LDI R17, 0b0000_0011
 	OUT PORTB, R17
-	//CALL IdelayT0
+	CALL IdelayT0
 	LDI R18, 0
 	//*******************************************************
 	//Apagar tx y rx
@@ -56,46 +56,48 @@ Setup:
 
 	LDI R19, 0xFF
 	OUT PORTD, R19
+
+	LDI R22, 0
+Mostrarvalorinicial:
+	LPM R18, Z
+	OUT PORTD, R18
 	//*******************************************************
 loop:
-	/*IN R16, TIFR0
+//TIMER
+	IN R16, TIFR0
 	CPI R16, (1 << TOV0)
-	BRNE loop
-	
-	LDI R16, 200
+	BREQ CHECK
+PrimerBoton: //Lectura del primer boton
+    IN R16, PINB
+	SBRS R16, PB0
+	JMP Pres1
+//******************************************
+SegundoBoton: //Lectura del segundo boton
+	IN R16, PINB
+	SBRS R16, PB1
+	JMP Pres2
+
+	JMP loop
+//*****************************************************************************
+// Sub-rutinas
+//*****************************************************************************
+CHECK:
+	LDI R16, 100
 	OUT TCNT0, R16
 	
 	SBI TIFR0, TOV0
 
-	INC R18
-	CPI R18, 25
+	INC R23
+	CPI R23, 100
 	BRNE loop
-	CLR R18
+	CLR R23
 
 	INC R17
 	OUT PORTC, R17
 	CPI R17, 16
 	BRNE loop
-	CLR R17 */
-
-PrimerBoton: //Lectura del primer boton
-    IN R16, PINB
-	SBRS R16, PB0
-	CALL Pres1
-//******************************************
-SegundoBoton: //Lectura del segundo boton
-	IN R16, PINB
-	SBRS R16, PB1
-	CALL Pres2
-Mostrarvalorinicial:
-	LPM R18, Z
-	OUT PORTD, R18
-	RJMP loop
-//*****************************************************************************
-// Sub-rutinas
-//*****************************************************************************
+	CLR R17 
 IdelayT0:
-
 	LDI R16, (1 << CS02) | (1 << CS00)
 	OUT TCCR0B, R16
 
@@ -116,36 +118,34 @@ Pres1: //Funcion luego de detectar un boton presionado
 	NOP
 	CALL delay //Espera el delay para no sumar valores fantasmas
 	SBIS PINB, PB0 //Sigue la funcion hasta que el usuario haya soltado el boton
-	JMP Pres1 //Si aun no lo suelta regresa al incio del Delay
-	RJMP incre //Si ya lo solto podemos sumar el valor ya que estamos seguros de que no es un valor fantasma
+	RJMP Pres1 //Si aun no lo suelta regresa al incio del Delay
+	JMP incre //Si ya lo solto podemos sumar el valor ya que estamos seguros de que no es un valor fantasma
 
 Pres2:
 	NOP
 	CALL delay
 	SBIS PINB, PB1
-	JMP Pres2
-	RJMP decre
+	RJMP Pres2
+	JMP decre
 
 incre:
     INC R19 //Incrementa la cuenta de la primera fila de leds
     SBRC R19, 4 // Limitar el contador a 4 bits
     CALL OVERFLO
 	ADD ZL, R21
-	RJMP LEDS1
-	RJMP loop
+	JMP LEDS1
 
 decre:
 	DEC R19 //Decrementa la cuenta de la primera fila de leds
     SBRC R19, 7 // Limitar el contador a no tener numeros negativos
 	CALL OVERFLO2
 	SUB ZL, R21
-	RJMP LEDS1
-	RJMP loop
+	JMP LEDS1
 
 LEDS1:
 	LPM R18, Z
 	OUT PORTD, R18
-	RJMP loop
+	JMP loop
 
 OVERFLO:
 	CLR R19
